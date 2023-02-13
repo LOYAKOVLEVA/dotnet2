a1b73d20a3befd6a0205c0d6e155161a771ece57
Yakovleva Larisa <lara.yakovleva.98.98@mail.ru>
2023-02-13 17:49:49 +0700

    четвертый коммит

---
+++ zad3/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите первое число");
+int i = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (i > a)
+{
+    Console.WriteLine("max = " + i);
+}
+else if (a > i)
+{
+    Console.WriteLine("max = "+ a);
+}
