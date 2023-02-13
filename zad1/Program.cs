9466a55f2e2920d86f764252bc187eadd9f4a44a
Yakovleva Larisa <lara.yakovleva.98.98@mail.ru>
2023-02-13 17:14:24 +0700

    второй коммит

--- zad1/Program.cs
+++ zad1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i <= a; i ++)
+{
+    if (i % 2 == 0 && i != 0)
+    {
+        Console.WriteLine(i);
+    }
+}
+
