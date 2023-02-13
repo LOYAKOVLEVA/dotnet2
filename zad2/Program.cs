376be87890eae1f16af806e0880aa7320f53cee1
Yakovleva Larisa <lara.yakovleva.98.98@mail.ru>
2023-02-13 17:28:27 +0700

    третий коммит

---
+++ zad2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(b % 2 == 0)
+{
+    Console.WriteLine("Четное число");
+}
+else
+{
+    Console.WriteLine("Нечетное число");
+}
